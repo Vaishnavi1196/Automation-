b26855839a1811e5d910d5e6444b7e43ee816d52
SHUBHAM GADPAYLE <SHUBHAM GADPAYLE@Shubham>
2022-07-11 17:28:11 +0530

    By Shubham

---
+++ Git2/src/A.java
@@ -0,0 +1,11 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Main method");
+		m2();
+	}
+	public static void m2() {
+		System.out.println("this is second method");
+	}
+}
