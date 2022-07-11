284a068761a14abcee41738f92a90d50fbea505f
Vaishnavi1196 <107459022+Vaishnavi1196@users.noreply.github.com>
2022-07-11 17:32:10 +0530

    Update A.java

--- Git2/src/A.java
+++ Git2/src/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Main method");
 		m2();
+		m3();
 	}
 	public static void m2() {
 		System.out.println("this is second method");
